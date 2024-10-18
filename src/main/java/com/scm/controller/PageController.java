8ea3de3be325b2c866321b31eb1bd55bedd27829
sumit sengar <87333260+sengarsumit@users.noreply.github.com>
2024-10-18 12:13:41 +0530

    register page

--- src/main/java/com/scm/controller/PageController.java
+++ src/main/java/com/scm/controller/PageController.java
@@ -2,6 +2,7 @@ package com.scm.controller;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,4 +30,20 @@ public class PageController {
         System.out.println("services page loading");
         return "services";
     }
+    @GetMapping("/contact")
+    public String contact()
+    {
+        return new String("contact");
+    }
+    @GetMapping("/login")
+    public String login()
+    {
+        return new String("login");
+    }
+    @GetMapping("/register")
+    public String register()
+    {
+        return new String("register");
+    }
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./src/main/resources/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-  darkmode: "selector",
-}
+  darkMode: "selector",
+};
